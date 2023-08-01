3ed3e624d7a2c9126e087bb7a7bb25350d6ea647
mfontenelle <57328336+mfontenelle@users.noreply.github.com>
2023-08-01 10:09:01 -0300

    Update Scopes.java
    comentario

--- T4/src/main/java/br/ufscar/dc/compiladores/LA/Scopes.java
+++ T4/src/main/java/br/ufscar/dc/compiladores/LA/Scopes.java
@@ -12,18 +12,22 @@ public class Scopes {
         createNewScope();
     }
 
+    //cria novo escopo
     public void createNewScope() {
         tablesStack.push(new SymbolTable());
     }
 
+    //obtem tabela de simbolos do escopo atual
     public SymbolTable getCurrentScope() {
         return tablesStack.peek();
     }
 
+    //retorna a lista de tabelas de simbolo, representando escopos aninhados
     public List<SymbolTable> runNestedScopes() {
         return tablesStack;
     }
 
+    //remove escopo atual
     public void giveupScope() {
         tablesStack.pop();
     }
