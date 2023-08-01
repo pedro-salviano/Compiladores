56353db918cc1335a97604b3f918c1fdd655e858
mfontenelle <57328336+mfontenelle@users.noreply.github.com>
2023-08-01 10:17:20 -0300

    Update SymbolTableEntry.java
    comentarios

--- T4/src/main/java/br/ufscar/dc/compiladores/LA/SymbolTableEntry.java
+++ T4/src/main/java/br/ufscar/dc/compiladores/LA/SymbolTableEntry.java
@@ -2,10 +2,10 @@ package br.ufscar.dc.compiladores.LA;
 
 public class SymbolTableEntry {
     public String name;
-    public SymbolTable.TypeLAIdentifier identifierType;
-    public SymbolTable.TypeLAVariable variableType;
+    public SymbolTable.TypeLAIdentifier identifierType; //tipo de identif
+    public SymbolTable.TypeLAVariable variableType; //tipo de variavel
 
-    public SymbolTable argsRegFunc;
-    public String functionType;
+    public SymbolTable argsRegFunc; //tabela de simbolos que armazena os argumentos de uma func ou registro
+    public String functionType; // retorno da func representada pela tabela de simbolos
 
 }
